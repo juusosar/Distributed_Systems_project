cc1e4845c9909d4cd67547b840f8d7ae64b1e73a
Juuso Herajärvi <juusoherajarvi@hotmail.com>
2024-03-01 10:57:37 +0200

    Updated README and database code

---
+++ Battleship/schema.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id int PRIMARY KEY,
+    username varchar(25) NOT NULL,
+    hashed_password varchar(256) NOT NULL,
+    salt varchar(100) NOT NULL,
+    registration_date date
+);
+
+DROP TABLE IF EXISTS user_game_stats;
+CREATE TABLE user_game_stats (
+    user_id int,
+    games_played int,
+    won int,
+    lost int,
+    win_percentage AS (won/games_played),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
