1b6d12d7433b2373b155a3e90d3823a4a46e276f
Juuso Herajärvi <juusoherajarvi@hotmail.com>
2024-03-03 15:42:58 +0200

    Added hash_password method and database functionality to register and login

--- Battleship/server.py
+++ Battleship/server.py
@@ -1,9 +1,13 @@
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from database import Database
+from datetime import date
+import bcrypt
 
 app = Flask(__name__)
 session = {}
+db = Database('database.db')
 
 
 @app.route("/")
@@ -14,13 +18,27 @@ def login():
         username = request.form['name']
         password = request.form['password']
 
-        # TODO: Check user and password from database
+        # Check user and password from database
+        try:
+            db.connect()
 
-        session['loggedin'] = True
-        session['userid'] = username
-        message = 'Logged in successfully !'
-        return render_template('user.html',
-                               message=message)
+            if db.verify_user(username, password):
+                message = 'Logged in successfully !'
+                session['loggedin'] = True
+            else:
+                message = 'Invalid username or password.'
+                session['loggedin'] = False
+
+            session['userid'] = username
+
+        except sqlite3.Error as error:
+            message = 'Something went wrong !'
+            session['loggedin'] = False
+
+        finally:
+            db.close()
+
+        return render_template('user.html', message=message)
     else:
         # message = 'Please enter username / password !'
         return render_template('login.html', message=message)
@@ -39,17 +57,41 @@ def register():
     if request.method == 'POST' and 'name' in request.form and 'password' in request.form:
         username = request.form['name']
         password = request.form['password']
-        account = True
 
-        # TODO: Upload username and password to database
-
-        if account:
+        # Checking if user already exists
+        username_check = False
+        try:
+            db.connect()
+            username_check = db.check_user(username)
+        except sqlite3.Error as error:
+                message = 'Something went wrong !'
+        finally:
+                db.close()
+        
+        if username_check:
             message = 'Account already exists !'
+
+        # TODO: Password requirements check
 
         elif not username or not password:
             message = 'Please fill out the form !'
+        
+        else:
+            # Current implementation of adding users
+            today = date.today()
+            hashed_password, salt = hash_password(password)
+            try:
+                db.connect()
+                db.add_user(username, hashed_password, salt, today)
+                db.connection.commit()
+                message = 'You have successfully registered!'
 
-        message = 'You have successfully registered!'
+            except sqlite3.Error as error:
+                message = 'Something went wrong !'
+
+            finally:
+                db.close()
+
         return render_template('login.html', message=message)
 
     elif request.method == 'POST':
@@ -78,6 +120,11 @@ def get_cookie():
     name = request.cookies.get('userID')
     return '<h1>Welcome to The Game, ' + name + '</h1>'
 
+# Function to hash a password
+def hash_password(password):
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password, salt
 
 if __name__ == '__main__':
     app.run(debug=True)
